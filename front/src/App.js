d9bc7864896cea630e3ae7dd64533169f0f522d6
ali-khosravani <ali.p3soft@gmail.com>
2023-12-15 09:41:28 +0330

    Remove react mark and add shekofaland

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          دوربین مداربسته -گیت کنترل تردد-سامانه های جامع حضور و غیاب و پارکینگ
         </p>
         <a
           className="App-link"
@@ -15,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          شکوفالند
         </a>
       </header>
     </div>
