f642bb7c1bad4f67089b92ce2cb01b53157f77e6
ali-khosravani <ali.p3soft@gmail.com>
2023-12-26 21:18:33 +0330

    Update UI

--- front/src/Component/Footer/Footer.jsx
+++ front/src/Component/Footer/Footer.jsx
@@ -1,17 +1,15 @@
 import './Footer.css'
 
 function Footer() {
-    return (
-        <div className='footer'>
+    return (        
             <div className='container-fluid'>
                 <div className='row'>
-                    <div className='footer-1 col-sm-12 col-md-6 col-lg-3 pt-3'>راهنمای سریع</div>
-                    <div className='footer-1 col-sm-12 col-md-6 col-lg-3 pt-3'>دسترسی</div>
-                    <div className='footer-1 col-sm-12 col-md-6 col-lg-3 pt-3'>قوانین</div>
-                    <div className='footer-1 col-sm-12 col-md-6 col-lg-3 pt-3'>مجوز</div>
+                    <div className='footer justify-content-center  text-center col-sm-12 col-md-6 col-lg-3 pt-3'>راهنمای سریع</div>
+                    <div className='footer justify-content-center  text-center col-sm-12 col-md-6 col-lg-3 pt-3'>دسترسی</div>
+                    <div className='footer justify-content-center  text-center col-sm-12 col-md-6 col-lg-3 pt-3'>قوانین</div>
+                    <div className='footer justify-content-center  text-center col-sm-12 col-md-6 col-lg-3 pt-3'>مجوز</div>
                 </div>
-            </div>
-        </div>
+            </div>        
     );
 }
 export default Footer;
--- front/src/Component/Header/Header.jsx
+++ front/src/Component/Header/Header.jsx
@@ -1,35 +1,33 @@
 import './Header.css'
 import home from './../../../src/assets/icon/house-fill.svg';
-import logo from './../../../src/assets/icon/Attendance.png';
+
 
 function Header() {
-  return(
-      <div className="MainHeader position-fixed" dir='rtl'>
-      <div className='container position-relative d-flex justify-content-between h-100'>
-        <div className='position-absolute h-100 pr-3'></div>
-        
-        <div className='w-100 d-lg-flex align-items-center'>
-            <div className='main-menu ' dir='rtl'>
-              <ul className='d-flex justify-content-center align-content-center list-unstyled p-0 m-0 h-100'>
-                  <li><a href='/'><img src={home} alt='No image'/></a></li>
-                  <li><a href='/'>کنترل تردد</a></li>
-                  <li><a href='/'>نرم افزار</a></li>
-                  <li><a href='/'>پشتیبانی</a></li>
-                  <li><a href='/'>بلاگ</a></li>
-                  <li><a href='/'>ارتباط با ما</a></li>
-                  <li><a href='/'>درباره ما</a></li>       
-              </ul>
-            </div>    
-        </div>  
-        <div className='d-flex align-itemes-center' dir='rtl'>
-            <div className='d-flex align-items-center'>
-                <a href='#'><img src={logo} height='30' width='auto' alt='No-image'/></a>
-            </div>
-        </div>          
-      </div>
-    </div>
+  return (
+    <nav class='navbar navbar-expand-md fixed-top'>
+      <div class="container-fluid pt-5">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="MainHeader collapse navbar-collapse">
+          <div className='container position-relative d-flex justify-content-between h-100'>            
+              <div className='main-menu ' dir='rtl'>
+                <ul className='d-flex list-unstyled navbar-nav'>
+                  <li className='nav-item'><a href='/'><img src={home} alt='Home' width={24} height={24}/></a></li>
+                  <li className='nav-item'><a href='/'>کنترل تردد</a></li>
+                  <li className='nav-item'><a href='/'>نرم افزار</a></li>
+                  <li className='nav-item'><a href='/'>پشتیبانی</a></li>
+                  <li className='nav-item'><a href='/'>بلاگ</a></li>
+                  <li className='nav-item'><a href='/'>ارتباط با ما</a></li>
+                  <li className='nav-item'><a href='/'>درباره ما</a></li>
+                </ul>
+              </div>
+            </div>          
+          </div>
+        </div>
+    </nav>
   )
-  
+
 }
 
 export default Header;
