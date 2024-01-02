906a1d512b70261d3ad5be6741f2edc1ab68de2c
ali-khosravani <ali.p3soft@gmail.com>
2024-01-02 23:05:54 +0330

    Update Carousel UI

--- front/src/Component/SocialMedia/Carousel.jsx
+++ front/src/Component/SocialMedia/Carousel.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './SocialMedia.css'
+
 import Parking from '../../assets/icon/Parking.png'
 import SelfService from '../../assets/icon/SelfService.png'
 import Attendance from '../../assets/icon/Attendance.png';
@@ -11,27 +13,31 @@ function Carousel() {
         <div className="row">
                 <div className="col-lg-3 col-md-6">
                 <img src={Attendance} alt='Home' width={96} height={96} />
-                    <h2 className="fw-normal">کنترل تردد</h2>
-                    <p>راهکاری جامع برای کنترل تردد در سازمان شما</p>
-                    <p><a className="btn btn-secondary" href="#">بیشتر &raquo;</a></p>
+               
+                    <h2 className="fw-normal SocialMedia-font pt-3">کنترل تردد</h2>
+                    <p className="SocialMedia-Para">راهکاری جامع برای کنترل تردد در سازمان شما</p>
+                    <p className="SocialMedia-Button"><a className="btn btn-secondary" href="#">بیشتر &raquo;</a></p>
                 </div>
                 <div className="col-lg-3 col-md-6">
                 <img src={Visitor} alt='Home' width={96} height={96} />
-                    <h2 className="fw-normal">مراجعین</h2>
-                    <p>راهنمایی مراجعه کننده به سازمان شما و عدم معطلی ارباب رجوع</p>
-                    <p><a className="btn btn-secondary" href="#">بیشتر &raquo;</a></p>
+
+                    <h2 className="fw-normal SocialMedia-font pt-3">مراجعین</h2>
+                    <p className="SocialMedia-Para">راهنمایی مراجعه کننده به سازمان شما و عدم معطلی ارباب رجوع</p>
+                    <p className="SocialMedia-Button"><a className="btn btn-secondary" href="#">بیشتر &raquo;</a></p>
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <img src={Parking} alt='Home' width={96} height={96} />
-                    <h2 className="fw-normal">پارکینگ</h2>
-                    <p>نرم افزاری همه منظوره برای مدیریت پارکینگ ها</p>
-                    <p><a className="btn btn-secondary" href="#">بیشتر &raquo;</a></p>
+                
+                    <h2 className="fw-normal SocialMedia-font pt-3">پارکینگ</h2>
+                    <p className="SocialMedia-Para">نرم افزاری همه منظوره برای مدیریت پارکینگ ها</p>
+                    <p className="SocialMedia-Button"><a className="btn btn-secondary" href="#">بیشتر &raquo;</a></p>
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <img src={SelfService} alt='Home' width={96} height={96} />
-                    <h2 className="fw-normal">سلف غذا خوری</h2>
-                    <p>بهترین راهکار برای مدیریت سلف های غذا خوری</p>
-                    <p><a className="btn btn-secondary" href="#">بیشتر &raquo;</a></p>
+            
+                    <h2 className="fw-normal SocialMedia-font pt-3">سلف غذا خوری</h2>
+                    <p className="SocialMedia-Para">بهترین راهکار برای مدیریت سلف های غذا خوری</p>
+                    <p className="SocialMedia-Button"><a className="btn btn-secondary " href="#">بیشتر &raquo;</a></p>
                 </div>
             </div>
             </div>
